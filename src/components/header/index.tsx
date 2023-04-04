4f805b0dba441b3bf0600ee71e7fe5db1ddffa4a
abiliobitt <abilio.bjr@gmail.com>
2023-04-03 21:37:59 -0300

    feat: create mobile menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,33 @@
-import { FC } from 'react'
-import { LiknsList, Logo, NavBarContainer } from './styles'
+import { FC, useEffect, useState } from 'react'
+import { LiknsList, Logo, MobileBar, NavBarContainer } from './styles'
 
 import logo from '../../assets/logo.png'
 import menuData from '../../appData/menu'
+import { menu } from '../../assets/icons'
 
 interface NavBarProps {
     isMobile: boolean
 }
 
-const Navbar: FC<NavBarProps> = ({ isMobile }) => {    
+const Navbar: FC<NavBarProps> = ({ isMobile }) => {
+    const [isOpen, setIsOpen] = useState(false)
+    useEffect(() => {
+        if(window.innerWidth > 768) {
+            setIsOpen(true)
+        }
+      }, [])
     return (
         <NavBarContainer>
             <Logo>
-            <img src={logo} alt='' />
+                <img src={logo} alt='' />
             </Logo>
+            <MobileBar onClick={()=> setIsOpen(!isOpen)}>
+                <span>Navegação</span>
+                <img src={menu} alt="Menu Mobile" className='mobileMenuIcon' />
+            </MobileBar>
             <LiknsList>
             {
-                menuData.map( (menuItem, index) => (
+                isOpen && menuData.map( (menuItem, index) => (
                     <li key={`menu-${index}`}>
                         <a
                             href={menuItem.link}
--- src/components/header/styles.tsx
+++ src/components/header/styles.tsx
@@ -1,35 +1,71 @@
-import styled from "styled-components"
-import { GRAY_SCALE_0, PRIMARY, PRIMARY_HOVER } from "../general/colors"
+import styled from "styled-components";
+import { GRAY_SCALE_0, PRIMARY, PRIMARY_HOVER } from "../general/colors";
 
 export const NavBarContainer = styled.nav`
+  width: 100%;
+  padding: 20px 100px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  background: ${PRIMARY}b0;
+  position: fixed;
+  z-index: 5;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    text-align: center;
     width: 100%;
-    padding: 20px 100px;
-    box-sizing: border-box;
+    padding: 0;
+    z-index: 999;
+    background: ${PRIMARY};
+  }
+`;
+
+export const MobileBar = styled.div`
+display: none;
+@media (max-width: 768px) {
     display: flex;
+    color: ${GRAY_SCALE_0};
+    padding: 20px 10px;
     justify-content: space-between;
-    background: ${PRIMARY}b0;
-    position: fixed;
-    z-index: 5;
+    border: 1px solid ${GRAY_SCALE_0};
+    margin-top: 15px;
+    img {
+        width: 24px;
+    }
+}
+
 `
 
 export const Logo = styled.div`
-    img {
-        height: 80px;
-    }
-`
+  img {
+    height: 80px;
+  }
+`;
 
 export const LiknsList = styled.ul`
-    display: flex;
-    margin-top: 35px;
-    li {
-        a {
-            padding: 35px;
-            color: ${GRAY_SCALE_0};
-            text-decoration: none;
-            &:hover {
-                background: ${GRAY_SCALE_0}b0;
-                color: ${PRIMARY_HOVER};
-            }
-        }
+  display: flex;
+  margin-top: 35px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+    margin: 0;
+    text-align: center;
+  }
+  li {
+      a {
+          padding: 35px;
+          color: ${GRAY_SCALE_0};
+          text-decoration: none;
+          @media (max-width: 768px) {
+            width: 100%;
+            padding: 20px;
+            display: block;
+            border-bottom: 1px solid ${GRAY_SCALE_0}b0;
+          }
+      &:hover {
+        background: ${GRAY_SCALE_0}b0;
+        color: ${PRIMARY_HOVER};
+      }
     }
-`
+  }
+`;
